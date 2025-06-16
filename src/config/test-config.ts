55ca17e4de36e1891fcf57676297bd2f831de77e
krishpemmaraju <krishna.pemmaraju@wolseley.co.uk>
2025-06-16 14:10:27 +0100

    update test config file

--- src/config/test-config.ts
+++ src/config/test-config.ts
@@ -4,7 +4,7 @@ export const TEST_CONFIG = {
     TIMEOUTS: {
       global: 30 * 60 * 1000,
       test: 90 * 1000,
-      api: 25 * 1000,
+      api: 30 * 1000,
       element: 80 * 1000,
     }
 };
